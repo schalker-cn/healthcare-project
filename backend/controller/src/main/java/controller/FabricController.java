7f1abc315a8731915a938853b54055cfca30c740
Yiming Chen <ge83gix@tum.de>
2024-06-25 12:30:34 +0200

    update java beans

--- backend/controller/src/main/java/controller/FabricController.java
+++ backend/controller/src/main/java/controller/FabricController.java
@@ -1,18 +1,36 @@
 package controller;
-import org.hyperledger.fabric.client.Gateway;
-import org.hyperledger.fabric.client.GatewayException;
+import backend.models.Patient;
+import org.hyperledger.fabric.client.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
-@Restcontroller
+@RestController
 public class FabricController {
     @Autowired
-    private Contract contract;
+    @Qualifier("patientContract")
+    private Contract patientContract;
 
-    @PostMapping("/createAsset")
-    public String createAsset(@RequestBody Asset asset) {
+    @Autowired
+    @Qualifier("prescriptionContract")
+    private Contract prescriptionContract;
+
+    @Autowired
+    @Qualifier("medicineContract")
+    private Contract medicineContract;
+
+    @Autowired
+    @Qualifier("healthRecordContract")
+    private Contract healthRecordContract;
+
+    @Autowired
+    @Qualifier("doctorContract")
+    private Contract doctorContract;
+
+    @PostMapping("/createPatient")
+    public String createPatient(@RequestBody Patient patient) throws EndorseException, SubmitException, CommitStatusException, CommitException {
         try {
-            byte[] result = contract.submitTransaction("CreateAsset", asset.getId(), asset.getColor(), asset.getSize(), asset.getOwner(), asset.getValue());
+            byte[] result = patientContract.submitTransaction("CreatePatient", patient.getPatientID(), patient.getHospitalID(), patient.getName(), String.valueOf(patient.getAge()), patient.getGender(), patient.getEmail(), patient.getPhone(), patient.getAccessToDoctors());
             return new String(result);
         } catch (Exception e) {
             return "Error: " + e.getMessage();

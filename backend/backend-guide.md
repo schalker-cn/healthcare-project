4c36a0212f1111f3d5b721490b532dd05c341e96
Yiming <ge83gix@tum.de>
2024-06-25 19:37:35 +0200

    backend instruction

--- backend/backend-guide.md
+++ backend/backend-guide.md
@@ -1,1 +1,24 @@
-instructions on how to set up backend
+### instructions on how to set up backend (on vscode)
+1. install following extensions:
+    - Extension Pack for Java
+    - Gradle for Java
+
+2. download jdk and set up environment variable:
+    - download jdk 11 (in my case I download it from oracle)
+    - search for settings.json in the top search bar of vscode
+    - vscode checks jdk version by multiple env variables, make sure to modify all of them:
+        - "java.jdt.ls.java.home": "/path/to/jdk"
+        - "jdk.jdkhome": "/path/to/jdk"
+        - "java.configuration.runtimes": {
+            "name": "JavaSE-11",
+            "path": "/path/to/jdk",
+        }
+3. build backend application:
+    - open the project from root directory, containing both front and backend
+    - in ideal case, gradle icon is activated at the left side bar. Navigaate to gradle tab and click on the **"run a gradle build"** at the top. Enter **"build"** in the top search bar and the gradle will build the project
+
+4. start the network:
+    - follow instructions in /backend/network-settings/network-guide.md
+
+5. run backend application:
+    - navigate to /backend/controller/src/main/java/controller folder, and run application.java
--- backend/controller/src/main/java/controller/FabricGateway.java
+++ backend/controller/src/main/java/controller/FabricGateway.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,31 +9,34 @@ import java.security.cert.CertificateException;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
-import org.hyperledger.fabric.client.*;
+import org.hyperledger.fabric.client.Contract;
+import org.hyperledger.fabric.client.Gateway;
+import org.hyperledger.fabric.client.Network;
 import org.hyperledger.fabric.client.identity.Identities;
 import org.hyperledger.fabric.client.identity.Identity;
 import org.hyperledger.fabric.client.identity.Signer;
 import org.hyperledger.fabric.client.identity.Signers;
 import org.hyperledger.fabric.client.identity.X509Identity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
 
 import io.grpc.Grpc;
 import io.grpc.ManagedChannel;
 import io.grpc.TlsChannelCredentials;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FabricGateway {
+    // if ROOT_FOLDER env variable is not set, modify it manually here
+    private static final String ROOT_FOLDER = System.getenv().getOrDefault("FABRIC_ROOT", "/home/schalker/healthcare-project/healthcare-project");
     private static final String MSP_ID = System.getenv().getOrDefault("MSP_ID", "Org1MSP");
     private static final String CHANNEL_NAME = System.getenv().getOrDefault("CHANNEL_NAME", "mychannel");
     private static final String CHAINCODE_NAME = System.getenv().getOrDefault("CHAINCODE_NAME", "basic");
 
     // Path to crypto materials.
-    private static final Path CRYPTO_PATH = Paths.get("backend", "network-settings", "test-network", "organizations", "peerOrganizations", "org1.example.com");
+    private static final Path CRYPTO_PATH = Paths.get(ROOT_FOLDER, "backend", "network-settings", "test-network", "organizations", "peerOrganizations", "org1.example.com");
     // Path to user certificate.
     private static final Path CERT_DIR_PATH = CRYPTO_PATH.resolve(Paths.get("users/User1@org1.example.com/msp/signcerts"));
     // Path to user private key directory.

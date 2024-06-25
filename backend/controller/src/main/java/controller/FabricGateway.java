61b4bef6998ec5c03a661a324a64674e112494d7
Yiming Chen <ge83gix@tum.de>
2024-06-25 12:30:34 +0200

    update java beans

---
+++ backend/controller/src/main/java/controller/FabricGateway.java
@@ -0,0 +1,130 @@
+package controller;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.cert.CertificateException;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+import org.hyperledger.fabric.client.*;
+import org.hyperledger.fabric.client.identity.Identities;
+import org.hyperledger.fabric.client.identity.Identity;
+import org.hyperledger.fabric.client.identity.Signer;
+import org.hyperledger.fabric.client.identity.Signers;
+import org.hyperledger.fabric.client.identity.X509Identity;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+
+import io.grpc.Grpc;
+import io.grpc.ManagedChannel;
+import io.grpc.TlsChannelCredentials;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FabricGateway {
+    private static final String MSP_ID = System.getenv().getOrDefault("MSP_ID", "Org1MSP");
+    private static final String CHANNEL_NAME = System.getenv().getOrDefault("CHANNEL_NAME", "mychannel");
+    private static final String CHAINCODE_NAME = System.getenv().getOrDefault("CHAINCODE_NAME", "basic");
+
+    // Path to crypto materials.
+    private static final Path CRYPTO_PATH = Paths.get("../../network-settings/test-network/organizations/peerOrganizations/org1.example.com");
+    // Path to user certificate.
+    private static final Path CERT_DIR_PATH = CRYPTO_PATH.resolve(Paths.get("users/User1@org1.example.com/msp/signcerts"));
+    // Path to user private key directory.
+    private static final Path KEY_DIR_PATH = CRYPTO_PATH.resolve(Paths.get("users/User1@org1.example.com/msp/keystore"));
+    // Path to peer tls certificate.
+    private static final Path TLS_CERT_PATH = CRYPTO_PATH.resolve(Paths.get("peers/peer0.org1.example.com/tls/ca.crt"));
+
+    // Gateway peer end point.
+    private static final String PEER_ENDPOINT = "localhost:7051";
+    private static final String OVERRIDE_AUTH = "peer0.org1.example.com";
+
+    private final String assetId = "asset" + Instant.now().toEpochMilli();
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private static ManagedChannel newGrpcConnection() throws IOException {
+        var credentials = TlsChannelCredentials.newBuilder()
+                .trustManager(TLS_CERT_PATH.toFile())
+                .build();
+        return Grpc.newChannelBuilder(PEER_ENDPOINT, credentials)
+                .overrideAuthority(OVERRIDE_AUTH)
+                .build();
+    }
+
+    private static Identity newIdentity() throws IOException, CertificateException {
+        try (var certReader = Files.newBufferedReader(getFirstFilePath(CERT_DIR_PATH))) {
+            var certificate = Identities.readX509Certificate(certReader);
+            return new X509Identity(MSP_ID, certificate);
+        }
+    }
+
+    private static Signer newSigner() throws IOException, InvalidKeyException {
+        try (var keyReader = Files.newBufferedReader(getFirstFilePath(KEY_DIR_PATH))) {
+            var privateKey = Identities.readPrivateKey(keyReader);
+            return Signers.newPrivateKeySigner(privateKey);
+        }
+    }
+
+    private static Path getFirstFilePath(Path dirPath) throws IOException {
+        try (var keyFiles = Files.list(dirPath)) {
+            return keyFiles.findFirst().orElseThrow();
+        }
+    }
+
+    @Bean
+    public Gateway createGateway() throws Exception {
+        // The gRPC client connection should be shared by all Gateway connections to
+        // this endpoint.
+        var channel = newGrpcConnection();
+
+        var builder = Gateway.newInstance().identity(newIdentity()).signer(newSigner()).connection(channel)
+                // Default timeouts for different gRPC calls
+                .evaluateOptions(options -> options.withDeadlineAfter(5, TimeUnit.SECONDS))
+                .endorseOptions(options -> options.withDeadlineAfter(15, TimeUnit.SECONDS))
+                .submitOptions(options -> options.withDeadlineAfter(5, TimeUnit.SECONDS))
+                .commitStatusOptions(options -> options.withDeadlineAfter(1, TimeUnit.MINUTES));
+
+        try (var gateway = builder.connect()) {
+            return gateway;
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+
+    @Bean
+    public Network network(Gateway gateway) {
+        return gateway.getNetwork("mychannel");
+    }
+
+    @Bean(name = "patientContract")
+    public Contract patientContract(Network network) {
+        return network.getContract("PatientContract");
+    }
+
+    @Bean(name = "prescriptionContract")
+    public Contract prescriptionContract(Network network) {
+        return network.getContract("PrescriptionContract");
+    }
+
+    @Bean(name = "medicineContract")
+    public Contract medicineContract(Network network) {
+        return network.getContract("MedicineContract");
+    }
+
+    @Bean(name = "healthRecordContract")
+    public Contract healthRecordContract(Network network) {
+        return network.getContract("HealthRecordContract");
+    }
+
+    @Bean(name = "doctorContract")
+    public Contract doctorContract(Network network) {
+        return network.getContract("DoctorContract");
+    }
+}
